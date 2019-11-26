05f7d73609fbdfaa5eb3d677ab3a8f108d0975e3
wgx731 <wgx731@gmail.com>
2019-11-27 01:02:33 +0800

    [CICD]: add github action

--- scripts/build-artifacts.sh
+++ scripts/build-artifacts.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "[MOAI] source config ..."
+source ${PWD}/scripts/config.env || exit 1
+
 echo "[MOAI] building artifacts ..."
-$PWD/gradlew clean build -x test -x spotbugsMain -x spotbugsTest || exit 1
+$PWD/gradlew clean build -x test -x spotbugsMain -x spotbugsTest -q || exit 1
